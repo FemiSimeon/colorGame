2cc0e5518aeeffa1df55ccec2a2d3d47f7e1538f
Oladipupo Oluwafemi <oladipupooluwafemi80@gmail.com>
2025-09-08 17:44:27 +0100

    initial commit

---
+++ colorGame.js
@@ -0,0 +1,62 @@
+var colors = generateRandomColors(6);
+var givenColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var squares = document.querySelectorAll(".square");
+var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+var resetButton = document.getElementById("reset");
+
+colorDisplay.textContent = givenColor;
+
+resetButton.addEventListener("click", function () {
+  colors = generateRandomColors(6);
+  givenColor = pickColor();
+  colorDisplay.textContent = givenColor;
+  for (i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+  resetButton.textContent = "New Colors";
+});
+
+for (i = 0; i < squares.length; i++) {
+  squares[i].style.backgroundColor = colors[i];
+  squares[i].addEventListener("click", function () {
+    var clickedColor = this.style.backgroundColor;
+    if (clickedColor === givenColor) {
+      sameColor(givenColor);
+      h1.style.backgroundColor = givenColor;
+      resetButton.textContent = "Play Again?";
+      messageDisplay.textContent = "CORRECT!";
+    } else {
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again!";
+    }
+  });
+}
+
+function sameColor(color) {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function randomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function generateRandomColors(num) {
+  var arr = [];
+  for (i = 0; i < num; i++) {
+    arr.push(randomColor());
+  }
+  return arr;
+}
